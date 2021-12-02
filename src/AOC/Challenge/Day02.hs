a48b491d60750fce25ed52b0a4a462191775ba20
Justin Le <justin@jle.im>
2021-12-01 22:47:58 -0800

    day 2 in terms of semi direct product

--- src/AOC/Challenge/Day02.hs
+++ src/AOC/Challenge/Day02.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC.Challenge.Day02
 -- License     : BSD3
@@ -9,70 +6,59 @@
 -- Portability : non-portable
 --
 -- Day 2.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day02 (
     day02a
   , day02b
   ) where
 
-import           AOC.Prelude
+import           AOC.Common (traverseLines)
+import           AOC.Solver ((:~>)(..))
+import           Linear.V2
+import           Text.Read  (readMaybe)
 
-import qualified Data.Graph.Inductive           as G
-import qualified Data.IntMap                    as IM
-import qualified Data.IntSet                    as IS
-import qualified Data.List.NonEmpty             as NE
-import qualified Data.List.PointedList          as PL
-import qualified Data.List.PointedList.Circular as PLC
-import qualified Data.Map                       as M
-import qualified Data.OrdPSQ                    as PSQ
-import qualified Data.Sequence                  as Seq
-import qualified Data.Set                       as S
-import qualified Data.Text                      as T
-import qualified Data.Vector                    as V
-import qualified Linear                         as L
-import qualified Text.Megaparsec                as P
-import qualified Text.Megaparsec.Char           as P
-import qualified Text.Megaparsec.Char.Lexer     as PP
-
-day02a :: [(Point)] :~> _
+day02a :: [(V2 Int)] :~> _
 day02a = MkSol
-    { sParse = Just . mapMaybeLines (\x -> case words x of
-        "forward":n:_ -> Just $ (V2 (read n) 0)
-        "down":n:_ -> Just (V2 0 (read n))
-        "up":n:_ -> Just (V2 0 (negate (read n)))
-    )
+    { sParse = traverseLines $ parseAsDir
+            (\x -> V2 x 0)
+            (\y -> V2 0 y)
     , sShow  = show
-    , sSolve = Just . product . sum
+    , sSolve = Just . product @V2 . sum
     }
 
--- -   `down X` *increases* your aim by `X` units.
--- -   `up X` *decreases* your aim by `X` units.
--- -   `forward X` does two things:
---     -   It increases your horizontal position by `X` units.
---     -   It increases your depth by your aim *multiplied by* `X`.
+data SubState = SubState { pX :: !Int, pY :: !Int, aY :: !Int }
+  deriving stock Show
 
-day02b :: [Either Int Int] :~> _
+-- | semi-direct product
+instance Semigroup SubState where
+    SubState x y a <> SubState x' y' a' =
+      SubState (x + x') (y + y' + x' * a) (a + a')
+
+instance Monoid SubState where
+    mempty = SubState 0 0 0
+
+day02b :: [SubState] :~> Int
 day02b = MkSol
-    { sParse = Just . mapMaybeLines (\x -> case words x of
-        "forward":n:_ -> Just $ Left (read n)
-        "down":n:_ -> Just $ Right (read n)
-        "up":n:_ -> Just $ Right . negate $ read n
-      )
+    { sParse = traverseLines $ parseAsDir
+        (\x -> SubState x 0 0)
+        (\a -> SubState 0 0 a)
     , sShow  = show
-    , sSolve = Just . product . fst . foldl' go (0, 0)
+    , sSolve = Just . summarize . mconcat
     }
   where
-    go (pos, aim) = \case
-      Left i -> (pos + V2 i (i*aim), aim)
-      Right i -> (pos, aim + i)
+    summarize (SubState x y _) = x * y
+
+parseAsDir
+    :: (Int -> r)   -- ^ forward
+    -> (Int -> r)   -- ^ up or down
+    -> String
+    -> Maybe r
+parseAsDir f g ln = do
+    dir:n:_ <- Just $ words ln
+    amnt    <- readMaybe n
+    case dir of
+      "forward" -> Just $ f amnt
+      "down"    -> Just $ g amnt
+      "up"      -> Just $ g (-amnt)
+      _         -> Nothing
+
