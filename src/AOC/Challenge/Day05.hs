a1c56da8ca9f7b6a68b0b1b46fab32199da21d13
Justin Le <justin@jle.im>
2021-12-05 20:54:26 -0800

    in progress day 5

--- src/AOC/Challenge/Day05.hs
+++ src/AOC/Challenge/Day05.hs
@@ -29,9 +29,11 @@ module AOC.Challenge.Day05 (
 import           AOC.Prelude
 
 import           Data.Distributive
+import qualified Data.ExtendedReal              as ER
 import qualified Data.Graph.Inductive           as G
 import qualified Data.IntMap                    as IM
 import qualified Data.IntSet                    as IS
+import qualified Data.IntegerInterval           as I
 import qualified Data.List.NonEmpty             as NE
 import qualified Data.List.PointedList          as PL
 import qualified Data.List.PointedList.Circular as PLC
@@ -45,6 +47,26 @@ import qualified Linear                         as L
 import qualified Text.Megaparsec                as P
 import qualified Text.Megaparsec.Char           as P
 import qualified Text.Megaparsec.Char.Lexer     as PP
+
+-- data LineType = Horiz | Vert | Upwards | Downwards
+-- data Line = Line
+--     { lType :: LineType
+--     , lAxis :: Integer              -- ^ y for horiz, x for vert, x+y for up, x-y for down
+--     , lRange :: I.IntegerInterval
+--     }
+
+-- overlaps :: Line -> Line -> [V2 Integer]
+-- overlaps (Line t1 a1 r1) (Line t2 a2 r2) = case (t1, t2) of
+--     (Horiz, Horiz)
+--       | a1 == a2  ->
+--           let r3 = I.intersection r1 r2
+--           in  case (I.lowerBound r3, I.upperBound r3) of
+--                 (ER.Finite x, ER.Finite y) -> V2 a1 <$> [x..y]
+--                 _                          -> []
+--       | otherwise -> []
+--     (Horiz, Vert)
+--       | a1 `I.member` r2 || a2 `I.member` r1 -> [V2 a1 a2]
+--       | otherwise -> []
 
 parseLine :: String -> Maybe (V2 Point)
 parseLine = (traverse . traverse) readMaybe
