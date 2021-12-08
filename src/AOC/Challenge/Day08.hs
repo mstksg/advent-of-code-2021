4473d8c5c50133b33088cad0fd6a21231a277b1c
Justin Le <justin@jle.im>
2021-12-08 01:22:20 -0800

    day 8 with pre cacheing

--- src/AOC/Challenge/Day08.hs
+++ src/AOC/Challenge/Day08.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC.Challenge.Day08
 -- License     : BSD3
@@ -9,132 +6,64 @@
 -- Portability : non-portable
 --
 -- Day 8.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day08 (
     day08a
   , day08b
-  , allPossibleWires
-  , lookupTrie
-  , toWires
-  , Trie(..)
   ) where
 
-import           AOC.Prelude
+import           AOC.Common      (listTup, traverseLines, mapMaybeSet, countTrue)
+import           AOC.Solver      ((:~>)(..))
+import           Control.Lens    (Prism', prism', preview)
+import           Control.Monad   ((<=<))
+import           Data.Bifunctor  (first)
+import           Data.Char       (chr, ord)
+import           Data.Finite     (Finite, finites, packFinite, getFinite)
+import           Data.List       (permutations)
+import           Data.List.Split (splitOn)
+import           Data.Map        (Map)
+import           Data.Maybe      (mapMaybe)
+import           Data.Set        (Set)
+import           Text.Read       (readMaybe)
+import qualified Data.Map        as M
+import qualified Data.Set        as S
 
-import qualified Data.Graph.Inductive           as G
-import qualified Data.IntMap                    as IM
-import qualified Data.IntSet                    as IS
-import qualified Data.List.NonEmpty             as NE
-import qualified Data.List.PointedList          as PL
-import qualified Data.DList.DNonEmpty as DNE
-import qualified Data.List.PointedList.Circular as PLC
-import           Data.Tuple.Strict
-import qualified Data.Map                       as M
-import qualified Data.OrdPSQ                    as PSQ
-import qualified Data.Sequence                  as Seq
-import qualified Data.Set                       as S
-import qualified Data.Text                      as T
-import qualified Data.Vector                    as V
-import qualified Linear                         as L
-import qualified Text.Megaparsec                as P
-import           Data.Functor.Foldable    (ana, hylo)
-import           Data.Functor.Foldable.TH (makeBaseFunctor)
-import qualified Text.Megaparsec.Char           as P
-import qualified Text.Megaparsec.Char.Lexer     as PP
+-- way too much type safety
 
+-- | Actual physical segment on the display
 newtype Segment = Segment { getSegment :: Finite 7 }
   deriving stock (Eq, Ord, Show)
 type Display = Set Segment
 
+-- | abcdefg
 newtype Wire = Wire { getWire :: Finite 7 }
   deriving stock (Eq, Ord, Show)
 type Wires = Set Wire
 
+-- | Map of wire (abcdefg) to segment
 type WireMap = Map Wire Segment
 
-_CharWire :: Prism' Char Wire
-_CharWire = prism'
-              (\(Wire w) -> chr $ fromIntegral (getFinite w) + ord 'a')
-              (\c -> Wire <$> packFinite (fromIntegral (ord c - ord 'a')))
-
-day08a :: _ :~> _
+day08a :: [(Set Wires, [Wires])] :~> Int
 day08a = MkSol
-    { sParse = traverseLines $
-         listTup . map words . splitOn " | "
+    { sParse = traverseLines parseLine
     , sShow  = show
-    , sSolve = Just . countTrue ((`elem` udigs) . length) . concatMap snd
+    , sSolve = Just . sum . map (countTrue isUnique . snd)
     }
   where
-    udigs = [2,4,3,7]
+    isUnique xs = length xs `S.member` uniques
+      where
+        uniques = S.fromList [2,4,3,7]
 
-mappings :: [Map Wire Segment]
-mappings = M.fromList . zip (Wire <$> finites) <$> permutations (Segment <$> finites)
-
-data Trie k a = TNode (Maybe a) (Map k (Trie k a))
-  deriving stock (Functor, Show)
-
-makeBaseFunctor ''Trie
-
-insertTrie
-    :: Ord k
-    => (a -> a -> a)        -- ^ new, old
-    -> [k]
-    -> a
-    -> Trie k a
-    -> Trie k a
-insertTrie f ks0 x = go ks0
-  where
-    go [] (TNode y ts) = TNode (Just $ maybe x (f x) y) ts
-    go (k:ks) (TNode y ts) = TNode y $
-      M.alter (Just . maybe (TNode (Just x) M.empty) (go ks)) k ts
-
-singletonTrie :: [k] -> a -> Trie k a
-singletonTrie ks0 x = go ks0
-  where
-    go [] = TNode (Just x) M.empty
-    go (k:ks) = TNode Nothing $ M.singleton k (go ks)
-
-lookupTrie :: Ord k => [k] -> Trie k a -> Maybe a
-lookupTrie [] (TNode x _) = x
-lookupTrie (k:ks) (TNode _ ts) = do
-    t <- M.lookup k ts
-    lookupTrie ks t
-
-trieFromListWith :: Ord k => (a -> a -> a) -> NonEmpty ([k], a) -> Trie k a
-trieFromListWith f = ana $ \xs ->
-    let T2 here there = flip foldMap xs $ \case
-          ([]  , a) -> T2 (Endo (a:)) mempty
-          (y:ys, a) -> T2 mempty (Endo ((y, DNE.singleton (ys, a)):))
-    in  TNodeF
-            (foldr1 f <$> NE.nonEmpty (appEndo here []))
-            (DNE.toNonEmpty <$> M.fromListWith (<>) (appEndo there []))
-    
-
-allPossibleWires :: Trie Wires (Set WireMap)
-allPossibleWires = trieFromListWith (<>) $ (second S.singleton) <$> NE.fromList poss
-  where
-    poss :: [([Wires], WireMap)]
-    poss = do
+-- | Map of all 9-digit observations to the wiremap that cuses them
+observationsMap :: Map (Set Wires) WireMap
+observationsMap = M.fromList do
       perm <- permutations $ Wire <$> finites
-      let mp = M.fromList $ zip (Segment <$> finites) perm
+      let mp  = M.fromList $ zip (Segment <$> finites) perm
           mp' = M.fromList $ zip perm (Segment <$> finites)
-          sigSet = sort $ S.map (mp M.!) <$> M.keys signals
-      visible <- catMaybes <$> traverse (\wrs -> [Nothing, Just wrs]) sigSet
+          visible = (S.map . S.map) (mp M.!) (M.keysSet signals)
       pure (visible, mp')
-    
 
-signals :: Map (Set Segment) Char
+signals :: Map Display Char
 signals = M.fromList . flip zip ['0'..'9'] . map (S.fromList . map Segment) $
     [ [0,1,2,4,5,6]
     , [2,5]
@@ -148,23 +77,35 @@ signals = M.fromList . flip zip ['0'..'9'] . map (S.fromList . map Segment) $
     , [0,1,2,3,5,6]
     ]
 
-toWires :: String -> Wires
-toWires = S.fromList . map (^?! _CharWire)
-
-day08b :: [([Wires], [Wires])] :~> [Int]
-day08b = MkSol
-    { sParse = traverseLines $
-         listTup . map (map (S.fromList . map (^?! _CharWire)) . words) . splitOn " | "
-    , sShow  = show . sum
-    , sSolve = traverse $ \xs -> firstJust (`decodeFromMapping` xs) mappings
-    }
+-- | Use a wiremap to turn a string into the integer it represents
+applyWireMap :: WireMap -> [Wires] -> Maybe Int
+applyWireMap mp = readMaybe <=< traverse decodeDigit
   where
-    decodeDigit :: Map Wire Segment -> Wires -> Maybe Char
-    decodeDigit mp = (`M.lookup` signals) . mapMaybeSet (`M.lookup` mp)
-    decodeString :: Map Wire Segment -> [Wires] -> Maybe Int
-    decodeString mp = readMaybe <=< traverse (decodeDigit mp)
-    decodeFromMapping :: Map Wire Segment -> ([Wires], [Wires]) -> Maybe Int
-    decodeFromMapping mp (xs, ys) = do
-      traverse_ (decodeDigit mp) xs
-      decodeString mp ys
+    decodeDigit = (`M.lookup` signals) . mapMaybeSet (`M.lookup` mp)
 
+day08b :: [(Set Wires, [Wires])] :~> [Int]
+day08b = MkSol
+    { sParse = traverseLines parseLine
+    , sShow  = show . sum
+    , sSolve = traverse $ \(xs, ys) -> do
+        mp <- M.lookup xs observationsMap
+        applyWireMap mp ys
+    }
+
+
+
+
+
+parseLine :: String -> Maybe (Set Wires, [Wires])
+parseLine = fmap (first S.fromList)
+          . listTup
+          . map (map toWires . words)
+          . splitOn " | "
+  where
+    toWires :: String -> Wires
+    toWires = S.fromList . mapMaybe (preview _CharWire)
+    -- | Parse a Char as a Wire, for type safety
+    _CharWire :: Prism' Char Wire
+    _CharWire = prism'
+                  (\(Wire w) -> chr $ fromIntegral (getFinite w) + ord 'a')
+                  (\c -> Wire <$> packFinite (fromIntegral (ord c - ord 'a')))
