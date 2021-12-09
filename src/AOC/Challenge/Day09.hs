fee89a284e7096da3a5336647b8e0ffbef347b9c
Justin Le <justin@jle.im>
2021-12-08 22:19:35 -0800

    loeb based part 2 for day 9, time down from 16s to 6s

--- src/AOC/Challenge/Day09.hs
+++ src/AOC/Challenge/Day09.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC.Challenge.Day09
 -- License     : BSD3
@@ -11,42 +8,27 @@
 -- Day 9.  See "AOC.Solver" for the types used in this module!
 --
 -- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day09 (
     day09a
   , day09b
   ) where
 
-import           AOC.Prelude
+import           AOC.Common       (freqs, digitToIntSafe)
+import           AOC.Common.Point (Point, cardinalNeighbs, parseAsciiMap)
+import           AOC.Solver       ((:~>)(..))
+import           Control.Monad    (mfilter)
+import           Data.Foldable    (toList)
+import           Data.List        (sortBy)
+import           Data.Map         (Map)
+import           Data.Maybe       (mapMaybe)
+import           Data.Ord         (comparing)
+import           Safe.Foldable    (minimumByMay)
+import qualified Data.Map         as M
 
-import qualified Data.Graph.Inductive           as G
-import qualified Data.IntMap                    as IM
-import qualified Data.IntSet                    as IS
-import qualified Data.List.NonEmpty             as NE
-import qualified Data.List.PointedList          as PL
-import qualified Data.List.PointedList.Circular as PLC
-import qualified Data.Map                       as M
-import qualified Data.OrdPSQ                    as PSQ
-import qualified Data.Sequence                  as Seq
-import qualified Data.Set                       as S
-import qualified Data.Text                      as T
-import qualified Data.Vector                    as V
-import qualified Linear                         as L
-import qualified Text.Megaparsec                as P
-import qualified Text.Megaparsec.Char           as P
-import qualified Text.Megaparsec.Char.Lexer     as PP
-
-findLow :: Map Point Int -> [(Point, Int)]
-findLow mp = filter go . M.toList $ mp
+-- | Find low points and their heights
+findLows :: Map Point Int -> [(Point, Int)]
+findLows mp = filter go . M.toList $ mp
   where
     go (p, i) = all isLow (cardinalNeighbs p)
       where
@@ -58,7 +40,7 @@ day09a :: Map Point Int :~> Int
 day09a = MkSol
     { sParse = Just . parseAsciiMap (mfilter (< 9) . digitToIntSafe)
     , sShow  = show
-    , sSolve = Just . sum . map ((+ 1) . snd) . findLow . M.filter (< 9)
+    , sSolve = Just . sum . map ((+ 1) . snd) . findLows . M.filter (< 9)
     }
 
 day09b :: Map Point Int :~> Int
@@ -66,13 +48,19 @@ day09b = MkSol
     { sParse = Just . parseAsciiMap (mfilter (< 9) . digitToIntSafe)
     , sShow  = show
     , sSolve = \xs -> Just
-        let lowpoints   = fst <$> findLow xs
-            validPoints = M.keysSet xs
-        in  product
-              . take 3
-              . sortBy (flip compare)
-              . map (S.size . floodFill (spreadout validPoints) . S.singleton)
-              $ lowpoints
+        let -- map of points to their associated low points after flowing
+            -- all the way downhill
+            res   = flip M.mapWithKey (flowMap xs) \p -> \case
+              Nothing -> p
+              Just q  -> res M.! q
+        in  product . take 3 . sortBy (flip compare) . toList $ freqs res
     }
+
+-- | Map of each point to the next point downhill.  If Nothing, then it's a low point.
+flowMap :: Map Point Int -> Map Point (Maybe Point)
+flowMap mp = M.mapWithKey go mp
   where
-    spreadout valids = (`S.intersection` valids) . cardinalNeighbsSet
+    go p i = fmap fst . minimumByMay (comparing snd) $
+               mapMaybe getGrad (cardinalNeighbs p)
+      where
+        getGrad q = (q,) <$> mfilter (< i) (M.lookup q mp)
